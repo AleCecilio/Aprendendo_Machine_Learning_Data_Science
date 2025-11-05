d8b41b4c07a5c3b43109d65df556effe2eb51841
Alessandro Cecilio <alessandro.m.cecilio@gmail.com>
2025-11-05 10:02:41 -0300

    Finalização da Tarefa 2 e do Módulo de Matplotlib.

--- Matplotlib/task_2.py
+++ Matplotlib/task_2.py
@@ -15,7 +15,7 @@ axes.plot(july16_2007, label='july16_2007', color='#344cd1')
 axes.plot(july16_2020, label='july16_2020' , color='orange')
 
 axes.set_xticks(range(len(labels)))
-axes.set_xticklabels(labels, rotation=45, ha='right')
+axes.set_xticklabels(labels)
 
 axes.legend(loc=(1.05,0.5))
 
@@ -35,7 +35,7 @@ axes[0].set_xticklabels(labels, rotation=45, ha='right')
 axes[1].plot(july16_2020)
 axes[1].set_title('July 16th, 2020')
 axes[1].set_xticks(range(len(labels)))
-axes[1].set_xticklabels(labels, rotation=45, ha='right')
+axes[1].set_xticklabels(labels)
 
 plt.tight_layout()
 plt.show()
@@ -44,28 +44,30 @@ plt.show()
 # FIGURA 3 — EIXOS DUPLOS (DUAS ESCALAS Y)
 fig3, axes1 = plt.subplots()
 
-axes1.plot(july16_2007, lw=2, color='blue')
+# Curva 2007 — eixo Y da esquerda (azul)
+axes1.plot(labels,july16_2007, lw=2, color='blue')
 axes1.set_ylabel('2007', fontsize=14, color='blue')
 
-for label in axes1.get_yticklabels():
-    label.set_color('blue')
-
-# Curva 2007 — eixo Y da esquerda (azul)
-axes1.set_title('July 16th Yield Curves')
-axes1.set_xticks(range(len(labels)))
-axes1.set_xticklabels(labels, rotation=45, ha='right')
 axes1.spines['left'].set_color('blue')
 axes1.spines['left'].set_linewidth(2)
 
+for label in axes1.get_yticklabels():
+    label.set_color('blue')
+plt.yticks(fontsize=12)    
+
+
 # Curva 2020 — eixo Y da direita (vermelho)
 axes2 = axes1.twinx()
-axes2.plot(july16_2020, lw=2, color='red')
+axes2.plot(labels, july16_2020, lw=2, color='red')
 axes2.set_ylabel('2020', fontsize=14, color='red')
-
-for label in axes2.get_yticklabels():
-    label.set_color('red')
 
 axes2.spines['right'].set_color('red')
 axes2.spines['right'].set_linewidth(2)
+
+for label in axes2.get_yticklabels():
+    label.set_color('red')
+plt.yticks(fontsize=12)  
+
+axes1.set_title('July 16th Yield Curves')
 
 plt.show()
