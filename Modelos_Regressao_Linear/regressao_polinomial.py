367cdc64568944668e17242eb2774948e8d00e87
Alessandro Cecilio <alessandro.m.cecilio@gmail.com>
2026-03-06 11:41:47 -0300

    Implementação da Regressão Polinomial com cálculo de MAE e RMSE no modelo.

--- Modelos_Regressao_Linear/regressao_polinomial.py
+++ Modelos_Regressao_Linear/regressao_polinomial.py
@@ -20,3 +20,23 @@ print('\n', X.iloc[0])
 print('\n', poly_features[0])
 
 print('\n', polynomial_converter.fit_transform(X))
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(poly_features, Y, test_size=0.3, random_state=101)
+
+from sklearn.linear_model import LinearRegression
+model = LinearRegression()
+model.fit(X_train, Y_train)
+test_predictions = model.predict(X_test)
+
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+MAE = mean_absolute_error(Y_test, test_predictions)
+MSE = mean_squared_error(Y_test, test_predictions) 
+RMSE = np.sqrt(MSE)
+
+print(f'\nMAE: {MAE}')
+print(f'RMSE: {RMSE}')
+
+print('\nCoefficients:', model.coef_)
+print('\n', poly_features[0])
+print('\n', X.iloc[0])
